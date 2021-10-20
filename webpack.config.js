4cb6590c98d058c2f1fab900997688be1a1c8404
salybu <rkdmsdl93@gmail.com>
2021-10-20 21:12:29 +0900

    feat: plugin 이용해 브라우저에 서버 띄우기

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // import
 const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
 
 // export
 module.exports = {
@@ -7,6 +8,19 @@ module.exports = {
   entry: "./js/main.js",
   // 결과물(번들)을 반환하는 설정
   output: {
-    path: path.resolve(__dirname, "dist"),
+    // path: path.resolve(__dirname, "public"),
+    // filename: "app.js",
+    clean: true,
   },
+
+  // 번들링 후 결과물의 처리방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: "./index.html",
+    }),
+  ],
+
+  // devServer: {
+  //   host: "localhost",
+  // },
 };
