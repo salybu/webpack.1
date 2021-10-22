062e4a96e97844277a8415a82e0ffddb62d5cdb7
salybu <rkdmsdl93@gmail.com>
2021-10-22 17:34:08 +0900

    feat: .css 파일에 css, style loader 설정

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,15 @@ module.exports = {
     clean: true,
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
   // 번들링 후 결과물의 처리방식 등 다양한 플러그인들을 설정
   plugins: [
     new HtmlPlugin({
